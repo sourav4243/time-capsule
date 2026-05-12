1520b4e214d92d6763c155b4de5a08bcb89afc11
sourav <souravkumar18835@gmail.com>
2026-05-12 10:49:48 +0530

    enhance time capsule functionality with signal handling and dynamic label updates

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import gi
+import signal
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, GLib, Gdk
+
+signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 class TimeCapsule(Gtk.Window):
     def __init__(self):
@@ -14,8 +17,10 @@ class TimeCapsule(Gtk.Window):
         self.total_seconds = 0  # stores otiginal timer value for reset
         self.edit_pos = 0
         self.edit_digits = [0, 0, 0, 0]
+        self.font_size = 30
 
         self.label = Gtk.Label(label="00:00")
+        self.label.set_use_markup(True)
         self.add(self.label)
 
         GLib.timeout_add(1000, self.tick)
@@ -40,35 +45,39 @@ class TimeCapsule(Gtk.Window):
 
     def editing_text(self):
         d = self.edit_digits
-        text = f"{d[0]}{d[1]}:{d[2]}{d[3]}"
-        pos = self.edit_pos
-        chars = [d[0], d[1], ':', d[2], d[3]]
+        chars = [str(d[0]), str(d[1]), ':', str(d[2]), str(d[3])]
         result = ""
         digit_index = 0
         for ch in chars:
             if ch == ':':
                 result += ':'
             else:
-                if digit_index == pos:
-                    result += f"[{ch}]"
+                if digit_index == self.edit_pos:
+                    result += f"<u>{ch}</u>"
                 else:
-                    result += str(ch)
+                    result += ch
                 digit_index += 1
         return result
+    
+    def set_label(self, text, markup=False):
+        if markup:
+            self.label.set_markup(text)
+        else:
+            self.label_set_text(text)
 
     def tick(self, *args):
         if self.state == "running":
             if self.mode == "stopwatch":
                 self.seconds += 1
-                self.label.set_text(self.seconds_to_text(self.seconds))
+                self.set_label(self.seconds_to_text(self.seconds))
             elif self.mode == "timer":
                 self.seconds -= 1
                 if self.seconds <= 0:
                     self.seconds = 0
                     self.state = "done"
-                    self.label.set_text("00:00")
+                    self.set_label("00:00")
                 else:
-                    self.label.set_text(self.seconds_to_text(self.seconds))
+                    self.set_label(self.seconds_to_text(self.seconds))
         return True
 
     def on_key_press(self, widget, event):
@@ -80,12 +89,20 @@ class TimeCapsule(Gtk.Window):
                 self.state = "editing"
                 self.edit_digits = [0, 0, 0, 0]
                 self.edit_pos = 0
-                self.label.set_text(self.editing_text())
+                self.set_label(self.editing_text(), markup=True)
             else:
                 self.mode = "stopwatch"
                 self.state = "idle"
                 self.seconds = 0
-                self.label.set_text("00:00")
+                self.set_label("00:00")
+
+        elif key == ord('+') or key == ord('='):
+            self.font_size = min(80, self.font_size + 2)
+            self.apply_styles()
+        
+        elif key ==  ord('-'):
+            self.font_size = max(10, self.font_size - 2)
+            self.apply_styles()
 
         elif key == ord(' '):
             if self.mode == "stopwatch":
@@ -110,7 +127,7 @@ class TimeCapsule(Gtk.Window):
                 self.seconds = self.total_seconds
                 if self.seconds > 0:
                     self.state = "idle"
-                    self.label.set_text(self.seconds_to_text(self.seconds))
+                    self.set_label(self.seconds_to_text(self.seconds))
 
         elif key ==  ord('r') or key == ord('R'):
             if self.mode == "stopwatch":
@@ -121,37 +138,37 @@ class TimeCapsule(Gtk.Window):
                 self.state = "editing"
                 self.edit_digits = [0, 0, 0, 0]
                 self.edit_pos = 0
-                self.label.set_text(self.editing_text())
+                self.set_label(self.editing_text(), markup=True)
 
         elif self.mode == "timer" and self.state == "editing":
             if key == Gdk.KEY_Left:
                 self.edit_pos = max(0, self.edit_pos - 1)
-                self.label.set_text(self.editing_text())
+                self.set_label(self.editing_text(), markup=True)
             elif key == Gdk.KEY_Right:
                 self.edit_pos = min(3, self.edit_pos + 1)
-                self.label.set_text(self.editing_text())
+                self.set_label(self.editing_text(), markup=True)
             elif key == Gdk.KEY_Up:
                 limits = [5, 9, 5, 9]
                 self.edit_digits[self.edit_pos] = (self.edit_digits[self.edit_pos] + 1) % (limits[self.edit_pos] + 1)
-                self.label.set_text(self.editing_text())
+                self.set_label(self.editing_text(), markup=True)
             elif key == Gdk.KEY_Down:
                 limits = [5, 9, 5, 9]
                 self.edit_digits[self.edit_pos] = (self.edit_digits[self.edit_pos] - 1) % (limits[self.edit_pos] + 1)
-                self.label.set_text(self.editing_text())
+                self.set_label(self.editing_text(), markup=True)
     
     def apply_styles(self):
-        css = b"""
-            window {
+        css = f"""
+            window {{
                 background-color: rgba(0, 0, 0, 0);
-            }
-            label {
+            }}
+            label {{
                 background-color: #1a1a1a;
                 color: #ffffff;
                 font-family: monospace;
-                font-size: 20px;
-                padding: 8px 20px;
-            }
-        """
+                font-size: {self.font_size}px;
+                padding: {self.font_size // 4}px {self.font_size // 1.5}px;
+            }}
+        """.encode()
         provider = Gtk.CssProvider()
         provider.load_from_data(css)
         Gtk.StyleContext.add_provider_for_screen(
